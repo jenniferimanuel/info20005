53b264d4385d6d87e0cd89dae295da96ad9d90f6
jenniferimanuel <jennifer.imanuel2006@gmail.com>
2026-05-19 15:54:08 +1000

    Project added

---
+++ Project 3/script.js
@@ -0,0 +1,23 @@
+// script for the SEARCH BAR
+//search input, select all product cards
+const searchInput = document.getElementById("searchInput");
+const products = document.querySelectorAll(".product-card");
+const form= document.querySelector(".search-form")
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+});
+
+searchInput.addEventListener ("keyup", function() {
+//so it's not case sensitive
+    const value = searchInput.value.toLowerCase();
+    
+    //loop through every product
+    products.forEach (function(product) {
+        //get product keyword
+        const productName = product.dataset.name.toLowerCase();
+        //check value if it matches
+        if (productName.includes(value)){
+            product.style.display = "block";}
+        else {product.style.display = "none";}
+    });
+});
