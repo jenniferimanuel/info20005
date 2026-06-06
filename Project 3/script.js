d5f4a3b53af8543d16f27341046a53e801805b14
jenniferimanuel <jennifer.imanuel2006@gmail.com>
2026-06-06 21:20:36 +1000

    fixing quantity js

--- Project 3/script.js
+++ Project 3/script.js
@@ -18,6 +18,18 @@
         });
      }
 
+// Size Buttons: choosing size 
+const sizeButtons = document.querySelectorAll(".size-btn");
+let selectedSize = "";
+sizeButtons.forEach(function(button){
+  button.addEventListener("click", function (){
+    sizeButtons.forEach(function(btn){
+        btn.classList.remove("selected");
+    });
+    button.classList.add("selected");
+    selectedSize = button.textContent;
+  });
+});
 
 //ADD to cart
 const addToCartButton = document.querySelector(".add-to-cart-button");
@@ -36,23 +48,81 @@ if (addToCartButton) {
             size: selectedSize
         };
         //save product into local storage
-        localStorage.setItem("cartItem",JSON.stringify(product));
+        let cart = JSON.parse(localStorage.getItem("cart")) || [];
+        cart.push(product);
+
+        console.log(product);
+        console.log(cart);
+
+        localStorage.setItem("cart", JSON.stringify(cart));
         //go to shopping cart page
         window.location.href="shopping-cart.html";
         });
 }
 // displaying CART ITEM
-const cartItem = JSON.parse(localStorage.getItem("cartItem"));
-if (cartItem && document.getElementById("cart-name")){
-    document.getElementById("cart-image").src=cartItem.image;
-    document.getElementById("cart-name").textContent=cartItem.name;
-    document.getElementById("cart-price").textContent="$" + cartItem.price;
-    document.getElementById("cart-size").textContent = "Size: " + cartItem.size;
-    document.getElementById("cart-quantity-value").textContent= cartItem.quantity;
-    document.getElementById("cart-subtotal").textContent="$" + (cartItem.price * cartItem.quantity);
+const cartItemsList = document.getElementById("cart-items-list");
+const cartSubtotal = document.getElementById("cart-subtotal");
+
+if (cartItemsList && cartSubtotal) {
+    const cart = JSON.parse(localStorage.getItem("cart")) || [];
+    let subtotal = 0;
+    cartItemsList.innerHTML = "";
+    cart.forEach(function(item, index) {
+        subtotal = subtotal + item.price * item.quantity;
+
+        cartItemsList.innerHTML += `
+        <div class="cart-item-card">
+                <img class="cart-product-image" src="${item.image}" alt="${item.name}">
+
+                <div class="cart-item-info">
+                <h2>${item.name}</h2>
+                <p>Size: ${item.size}</p>
+                <p class="cart-price">$${item.price}</p>
+
+                <div class="cart-quantity">
+                    <button type="button" onclick="decreaseCartQuantity(${index})">-</button>
+                    <span>${item.quantity}</span>
+                    <button type="button" onclick="increaseCartQuantity(${index})">+</button>
+                </div>
+
+                <button type="button" onclick="removeCartItem(${index})" class="remove-cart-btn">
+                Remove
+                </button>
+            </div>
+        </div>
+        `;
+    });
+    cartSubtotal.textContent = "$" + subtotal;
 }
 
-//Quantity Buttons
+//to increase and decrease quantity
+function increaseCartQuantity(index) {
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+    cart[index].quantity++;
+    localStorage.setItem("cart", JSON.stringify(cart));
+    location.reload();
+}
+
+function decreaseCartQuantity(index) {
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+    if (cart[index].quantity > 1) {
+        cart[index].quantity--;
+    }
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+    location.reload();
+}
+
+//REMOVE product
+function removeCartItem(index) {
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+    cart.splice(index, 1);
+    localStorage.setItem("cart", JSON.stringify(cart));
+    location.reload();
+}
+
+//QUANTITY Buttons
 const minusBtn = document.getElementById("minus-btn");
 const plusBtn = document.getElementById("plus-btn");
 const quantityValue = document.getElementById("quantity-value");
@@ -72,15 +142,3 @@ if (minusBtn && plusBtn && quantityValue) {
     });
 }
 
-// Size Buttons: choosing size 
-const sizeButtons = document.querySelectorAll(".size-btn");
-let selectedSize = "";
-sizeButtons.forEach(function(button){
-  button.addEventListener("click", function (){
-    sizeButtons.forEach(function(btn){
-        btn.classList.remove("selected");
-    });
-    button.classList.add("selected");
-    selectedSize = button.textContent;
-  });
-});
